fb4159bc6d250c35f93de5cf09f2f7957533070f
James Reinke <jamesreinke4@gmail.com>
2019-02-05 16:01:35 -0800

    ~

---
+++ Scala/01.scala
@@ -0,0 +1,8 @@
+/*
+	Scala is a blend of object-oriented and functional programming concepts in a statically typed language.
+*/
+
+// 1.1: A language that grows on you
+var capital = Map("US" -> "Washington", "France" -> "Paris")
+capital += ("Japan" -> "Tokyo")
+println(capital("France"))
